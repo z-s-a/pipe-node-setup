c93ae3b04dea2357ed942ac646d34a10f940eb37
z-s-a <sergeyzmdegen@gmail.com>
2025-02-07 15:05:02 +0300

    Первый коммит

---
+++ install_pipe_node.sh
@@ -0,0 +1,7 @@
+echo "# pipe-node-setup" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/z-s-a/pipe-node-setup.git
+git push -u origin main
